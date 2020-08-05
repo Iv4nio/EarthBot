22ec5994534b9717c8c0c850e49b499a4e058981
Iv4nio <33789051+Iv4nio@users.noreply.github.com>
2020-08-05 21:08:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const bot = new Discord.Client();
 const moment = require('moment');
 const ytdl = require('ytdl-core');
 const superagent = require('superagent')
+const db = require('quick.db');
+const readline = require('readline');
 require("moment-duration-format");
+
+var express = require('express');
+var app = express();
+app.get("/", (request, response) => {
+    response.sendStatus(200);
+});
+app.listen(process.env.PORT);
 
 const prefix = 'e!'
 
@@ -17,29 +26,35 @@ bot.on('message', message => {
           message.channel.send(':ping_pong: | **Pong!** ' + Math.round(bot.ping) + ' ms!')
       }
       if (msg === prefix + 'help') {
-          message.channel.send(`
-__**Help**__
-You are currently looking at the help section which includes all of the commands that you will be able to see
-
-__**📖 General Commands**__
-- **e!userinfo** - Shows your simple user information
-- **e!serverinfo** - Shows the server information
-- **e!ping** - Reveals the ping count
-- **e!invite** - Displays the invite link to invite me
-- **e!support** - Links the support server
-- **e!report** - Use this command to report members
-
-__**🏹 Fun Commands**__
-- **e!coinflip** - Flips a coin either heads or tales
-- **e!add** - Math calculations
-- **e!lmgtfy** - Shows an LMGTFY link supplied by text
-
-__**⚙️ Moderation Commands**__
-- **e!kick** - Mention somebody to kick
-- **e!ban** - Mention somebody to kick
-
-**NOTE:** I am relatively new which means I am still currently being developed`)
-
+          const embed = new Discord.RichEmbed()
+              .setColor('RANDOM')
+              .setTitle('Help')
+              .setDescription('The commands are categorized into different group e.g moderation.')
+              .setTimestamp()
+              .addField('e!ping', 'Displays the latency between the server and the bot')
+              .addField('e!serverinfo', 'Displays the server information')
+              .addField('e!userinfo', 'Displays your user information')
+              .addField('e!coinflip', 'Heads or tails?')
+              .addField('e!add', 'Mathematical calculations (e.g 1 2 3 4)')
+              .addField('e!lmgtfy', 'LMGTFY link (e.g e!lmgtfy Hello World!)')
+              .addField('e!8ball', 'Interactive discord 8ball!')
+              .addField('e!avatar', 'Displays only your avatar')
+              .addField('e!report', 'Use this command to become a snitch and report someone')
+              .addField('e!kick', 'Kick somebody')
+              .addField('e!ban', 'Ban somebody')
+              .addField('e!clear', 'Use this command to purge messages');
+        message.channel.send(embed);
+       return; 
+      }
+      if (msg === prefix + 'avatar') {
+        let member = message.mentions.users.first();
+        var embedAuthor = new Discord.RichEmbed()
+            .setTitle('Here is your avatar ' + message.author.username + " :")
+            .setImage(message.author.avatarURL)
+            .setTimestamp();
+        if(!member) return message.channel.send(embedAuthor)
+        
+        return;
       }
       if (msg === prefix + 'userinfo') {
         let user = message.mentions.users.first() || message.author;
@@ -92,9 +107,10 @@ __**⚙️ Moderation Commands**__
           if(!kUser) return message.channel.send("Can't find user!");
           let kReason = args.join(" ").slice(22);
           if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You cannot kick him/her because you do not have the permission to kick members");
+          if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("You cannot kick that member!");
 
           let kickEmbed = new Discord.RichEmbed()
-          .setDescription("[KICK]")
+          .setDescription("Kick")
           .setColor('#0095ff')
           .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
           .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
@@ -102,36 +118,65 @@ __**⚙️ Moderation Commands**__
           .addField("Time", message.createdAt)
           .addField("Reason", kReason);
 
-          let kickChannel = message.guild.channels.find(`name`, "🚨mod-log")
-          if(!kickChannel) return message.channel.send("Can't find the channel ''🚨mod-log'")
+          let kickChannel = message.guild.channels.find(`name`, "📜earthbot-log")
+          if(!kickChannel) return message.channel.send("Can't find the channel 📜earthbot-log")
 
           message.guild.member(kUser).kick(kReason);
           kickChannel.send(kickEmbed);
 
           return;
       }
+  
+      if (message.content.startsWith(prefix + '8ball')) {
+          let replies = ['As I see it, yes.',
+                         'Ask again later.',
+                         'Better not tell you now.',
+                         'Cannot predict now.',
+                         'Concentrate and ask again.',
+                          'Hell no.',
+                          'It is certain.',
+                          'Hell yes.',
+                          'Most likely.',
+                          'My reply is no.',
+                          'My sources say no.',
+                          'Outlook not so good.',
+                          'Outlook good.',
+                          'Reply hazy, try again.',
+                          'Signs point to yes.',
+                          'Very doubtful.',
+                          'Without a doubt.',
+                          'Yes.',
+                          'Yes – definitely.',
+                          'You may rely on it.']
+          
+          let result = Math.floor((Math.random() * replies.length) + 0);
+        
+          message.channel.send(':8ball: | ' + replies[result])
+      }
 
       if (message.content.startsWith(prefix + 'report')) {
           let messageArray = message.content.split(" ");
           let args = messageArray.slice(1);
-          let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-          let reason = args.join(" ").slice(22);
-
-          let reportEmbed = new Discord.RichEmbed()
-          .setDescription("New Report")
-          .setColor("#FF0000")
-          .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-          .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-          .addField("Channel", message.channel)
-          .addField("Time", message.createdAt)
-          .addField("Reason", reason);
-
-          let reportchannel = message.guild.channels.find(`name`, "📝reports");
-
-          message.delete().catch(O_o=>{});
-          reportchannel.send(reportEmbed);
-
-          return;
+          let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+          let reason = args.slice(1).join(' ');
+          let reports = message.guild.channels.find('name', 'manual-reports')
+          
+          if(!target) return message.channel.send('Please specify a member to report!');
+          if(!reason) return message.channel.send('You must include a reason for the report!');
+          if(!reports) return message.channel.send('Cannot find the reports channel');
+        
+          let embed = new Discord.RichEmbed()
+              .setColor('RANDOM')
+              .setThumbnail(target.user.avatarURL)
+              .addField('Reported Member', `${target.user.username} with an ID: ${target.user.id}`)
+              .addField('Reported By', `${message.author.username} with an ID: ${message.author.id}`)
+              .addField('Reported Time', message.createdAt)
+              .addField('Reported In', message.channel)
+              .addField('Reported Reason', reason)
+              .setFooter('Reported User Information', target.user.displayAvatarURL);
+        
+        message.channel.send(`${target} has been successfully reported by ${message.author} for ${reason}. Thank you for making this server a better place!`);
+        reports.send(embed);
 
       }
 
@@ -142,25 +187,39 @@ __**⚙️ Moderation Commands**__
           let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
           if(!bUser) return message.channel.send("Can't find user!");
           let bReason = args.join(" ").slice(22);
-          if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(":warning: | You cannot ban him/her because you do not have the permission to ban members");
+          if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You cannot ban the user because you have insufficient permissions to use this command!");
+          if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("This user cannot be banned!")
 
           let banEmbed = new Discord.RichEmbed()
-          .setDescription("[BAN]")
+          .setDescription("Ban")
           .setColor('#0095ff')
+          .setTimestamp()
           .addField("Banned User", `${bUser} with ID ${bUser.id}`)
           .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
           .addField("Banned In", message.channel)
           .addField("Time", message.createdAt)
           .addField("Reason", bReason);
 
-          let banChannel = message.guild.channels.find(`name`, "🚨mod-log")
-          if(!banChannel) return message.channel.send("Can't find the channel ''🚨mod-log'")
+          let banChannel = message.guild.channels.find(`name`, "bot-log")
+          if(!banChannel) return message.channel.send("Can't find the channel 'bot-log'")
 
           message.guild.member(bUser).ban(bReason);
           banChannel.send(banEmbed);
 
           return;
       }
+      
+      if (message.content.startsWith(prefix + 'clear')) {
+         if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You must have the ``MANAGE_MESSAGES`` permission!")
+         let messageArray = message.content.split(" ");
+         let args = messageArray.slice(1);
+        
+         if(!args[0]) return message.channel.send("You must enter a number!");
+         message.channel.bulkDelete(args[0]).then(() => {
+           message.channel.send(`Cleared ${args[0]} message(s)`).then(message => message.delete(5000));
+         });
+      }
+  
       if (message.content.startsWith(prefix + 'coinflip')) {
           var chance = Math.floor(Math.random() * 2);
           if(chance == 0) {
@@ -187,17 +246,16 @@ __**⚙️ Moderation Commands**__
 
         message.channel.send(`**<${link}>**`)
       }
-      if (message.content.startsWith(prefix + 'invite')) {
-          message.channel.send('Invite me here: **https://discordapp.com/oauth2/authorize?client_id=579426425015369750&scope=bot&permissions=2146958847**')
-      }
-      if (message.content.startsWith(prefix + 'support')) {
-          message.channel.send('If you wish to join the server I originate from, the link is here: **https://discord.gg/ypcQaDE**')
-      }
  });
 
- bot.on('ready', () => {
-    console.log('Bot is ready with the username: ' + bot.user.username)
-    bot.user.setActivity(`e!help | Creator: savage doge`, {type: "PLAYING"})
- });
+bot.on('ready', () => {
+  function randomStatus() {
+    let status = ["COVID-19", "CoronaEarth", "e!help", "Nothing", "MINECRAFT", "FORTNITE"]
+    let rstatus = Math.floor(Math.random() * status.length);
+    bot.user.setActivity(status[rstatus], {type: "PLAYING",});
+  };
+  setInterval(randomStatus, 20000)
+  console.log('Online.')
+});
 
- bot.login(process.env.BOT_TOKEN);
+ bot.login(process.env.TOKEN);
